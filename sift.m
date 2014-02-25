1c1cbadd267ea1ef6bf50d3a4ab820d19e29c5e4
Deema Alshamaa <alshamaa@cs.wisc.edu>
2014-02-25 11:18:59 -0600

    sift using the matlab toolbox that vikas mentions-- get the program to work then see if we need to implement SIFT ourselves

--- sift.m
+++ sift.m
@@ -1,4 +1,6 @@
-function sift(im1, im2)
+%http://www.vlfeat.org/overview/sift.html
+
+function [d1,d2,matches,scores] = sift(im1, im2)
 
 %reading images here for testing. should move to main 
 
@@ -8,14 +10,19 @@ I2 =  single(rgb2gray(im2double(imread(im2))));
 [f1, d1] = vl_sift(I1);
 [f2, d2] = vl_sift(I2);
 
-[matches, scores] = vl_ubcmatch(d1, d2, 5);
-subplot(1,2,1);
-imshow(I1);
-hold on;
-plot(f1(1,matches(1,:)), f1(2, matches(1,:)), 'b*');
+size(f1), size(d1)
+size(f2), size(d2)
 
-subplot(1,2,2);
-imshow(I2);
-hold on;
-plot(f2(1,matches(2,:)), f2(2, matches(2,:)), 'r*');
+[matches, scores] = vl_ubcmatch(d1, d2, 5);
+
+size(matches), size(scores)
+%subplot(1,2,1);
+%imshow(I1);
+%hold on;
+%plot(f1(1,matches(1,:)), f1(2, matches(1,:)), 'b*');
+
+%subplot(1,2,2);
+%imshow(I2);
+%hold on;
+%plot(f2(1,matches(2,:)), f2(2, matches(2,:)), 'r*');
 end
