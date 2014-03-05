dadddbe53cc71127a94a853d0ead10babfed4358
Stephen Simon Lagree <slagree@tux-61.cae.wisc.edu>
2014-03-05 17:08:19 -0600

    Fixed panorama for many images to map everything to one image.

--- feather_blend.m
+++ feather_blend.m
@@ -2,8 +2,18 @@ function [ mosaic ] = feather_blend( img1,img2 )
 %% assumes img1 image is on the left and img2 is on the right
 
 %%blends the middle 1/5th of the image at the moment
-beginBlend=floor(size(img1,2)/5)*2;
-endBlend=floor(size(img1,2)/5)*3;
+
+beginA=min(find(sum(img1(:,:,1))>0));
+beginB=min(find(sum(img2(:,:,1))>0));
+if (beginB<beginA)
+    temp=img1;
+    img1=img2;
+    img2=temp;
+end   
+beginBlend=min(find(sum(img2(:,:,1))>0));
+endBlend=max(find(sum(img1(:,:,1))>0));
+%beginBlend=floor(size(img1,2)/5)*2;
+%endBlend=floor(size(img1,2)/5)*3;
 
 mosaic=zeros(size(img1));
 for channel=1:3
@@ -16,7 +26,13 @@ for channel=1:3
     %%does a simple linear interpolation:  (1-w)*img1 + (w)*img2
     for j=beginBlend:endBlend
         for i=1:size(img1c,1)
-            final(i,j)= img1c(i,j)*(endBlend-j)/(endBlend-beginBlend) + img2c(i,j)*(j-beginBlend)/(endBlend-beginBlend); 
+            if (img1c(i,j)==0)
+                final(i,j)=img2c(i,j);
+            elseif (img2c(i,j)==0)
+                final(i,j)=img1c(i,j);
+            else
+                final(i,j)= img1c(i,j)*(endBlend-j)/(endBlend-beginBlend) + img2c(i,j)*(j-beginBlend)/(endBlend-beginBlend); 
+            end
         end
     end
     mosaic(:,:,channel)=final;
--- homography.m
+++ homography.m
@@ -1,119 +1,129 @@
-function H = homography(im1, im2)
+function H = homography(images,reference)
 
+img1 = imread(images{reference});
+
+%images=images([1:reference-1,reference+1:end]);
 %reading the images here for easy debugging. Should move them to main
-img1 = imread(im1);
-img2 = imread(im2);
+allH={};
+minU=1;
+maxU=size(img1,2);
+minV=1;
+maxV=size(img1,1);
+for i=1:length(images)
+    if (i==reference)
+        allH{i}=eye(3);
+        continue
+    end
+    if i<reference
+        j=i+1;
+    else
+        j=i-1;
+    end
+    img1 = imread(images{j}); 
+    img2 = imread(images{i});
 
-im1 = inverse_cylinderical_projection (img1,595,-0.15,0);
-im2 = inverse_cylinderical_projection (img2,595,-0.15,0);
+    img1 = inverse_cylinderical_projection (img1,595,-0.15,0);
+    img2 = inverse_cylinderical_projection (img2,595,-0.15,0);
 
-im1= im2single(im1);
-im2 = im2single(im2);
+    im1= im2single(img1);
+    im2 = im2single(img2);
 
-if size(im1,3) > 1, I1 = rgb2gray(im1) ; else I1 = im1 ; end
-if size(im2,3) > 1, I2 = rgb2gray(im2) ; else I2 = im2 ; end
-
-% get SIFT features
-[f1,d1] = vl_sift(I1) ;
-[f2,d2] = vl_sift(I2) ;
-
-% To Steve: we can play with the third parameter, it's 1.5 by default 
-[matches, scores] = vl_ubcmatch(d1,d2) ;
-
-num_matches = size(matches,2) ;
-
-% f1 and f2 both have a column for each keypoint. 
-% From the documentation: A keypoint is a disk of center f1(1:2), scale f(3) and orientation f(4)
-X1 = f1(1:2,matches(1,:)) ; X1(3,:) = 1 ;
-X2 = f2(1:2,matches(2,:)) ; X2(3,:) = 1 ;
-
-
-% nDLT using SVD to solve the least square problem that
-%Ransac depends on. Source: http://vision.ece.ucsb.edu/~zuliani/Research/RANSAC/docs/RANSAC4Dummies.pdf
-
-% We repeat 100 times to ensure robustness and then we take the best
-% solution
-for j = 1:100
-  
-  %vl_colsubset(x,n) returns a random subset y of n columns of x. It's order preserving and without replacement 
-  rand_cols = vl_colsubset(1:num_matches, 4) ;
-  A = [] ;
-  
-  % kron() is the Kronecker Tensor Product of two matrices A and B. The
-  % resulting matrix is formed by multiplying B by every element in A.
-  % function details: http://www.mathworks.com/help/matlab/ref/kron.html#bt0autl-2_1
-  % vl_hat returns a skew symmetric matrix
-  % cat concatenates arrays along the dimension specified in the first
-  % parameter
-  for i = rand_cols
-    A = cat(1, A, kron(X1(:,i)', vl_hat(X2(:,i)))) ;
-  end
-  
-  % Singular Value Decomposition
-  % The solution to the linear system (DLT) is the eigenvector associated
-  % with the smallest eigenvalue. 
-  %[U,S,V] = svd(A); X = V(:,end) source: http://www.cs.unc.edu/~lazebnik/spring09/assignment3.html
-  [U,S,V] = svd(A) ;
-  H{j} = reshape(V(:,9),3,3) ;
-
-  % Measuring the quality of this solution by computing the distance 
-  %between corresponding points AFTER applying the transformation
-  
-  X1_proj = H{j} * X1 ; 
-  deltaU = X1_proj(1,:)./X1_proj(3,:) - X2(1,:)./X2(3,:) ;
-  deltaV = X1_proj(2,:)./X1_proj(3,:) - X2(2,:)./X2(3,:) ;
-  
-  %the next instruction should first calculate the paranethesis then it
-  %compares the values to 36. If the value <36, the value in ok will be one
-  %(TRUE). otherwise it'll be 0 (False)
-  temp{j} = (deltaU.*deltaU + deltaV.*deltaV) < 4 ;
-  score(j) = sum(temp{j}) ;
+    H=calcHomography(im1,im2);
+    allH{i}=H;
 end
 
-[~, best] = max(score) ;
-H = H{best} ;
-
-temp2 = [1 size(im2,2) size(im2,2) 1 ; 1  1 size(im2,1) size(im2,1);
-        1 1 1 1 ] ;
+for i=1:length(images)
     
-%transforming X2 by inv(H) to get the reconstructed scene image
-temp2_proj = inv(H) * temp2 ;
+    if(i==reference)
+        continue
+    end
+    img2 = imread(images{i});
+    im2 = im2single(img2);
+    
+temp2 = [1 size(im2,2) size(im2,2) 1 ;...
+        1  1 size(im2,1) size(im2,1);...
+        1 1 1 1 ] ;
 
-%project each image onto the same surface then blend them together
+    H=allH{i};
+    inc=1;
+    if i>reference
+        inc=-1;
+    end
+    for ind=i+inc:inc:(reference-inc)
+        H=H*allH{ind};
+    end
+    %transforming X2 by inv(H) to get the reconstructed scene image
+    temp2_proj = inv(H) * temp2 ;
 
-temp2_proj(1,:) = temp2_proj(1,:) ./ temp2_proj(3,:) ;
-temp2_proj(2,:) = temp2_proj(2,:) ./ temp2_proj(3,:) ;
+    %project each image onto the same surface then blend them together
+    temp2_proj(1,:) = temp2_proj(1,:) ./ temp2_proj(3,:) ;
+    temp2_proj(2,:) = temp2_proj(2,:) ./ temp2_proj(3,:) ;
+    minU=min([minU,temp2_proj(1,:)]);
+    maxU=max([maxU,temp2_proj(1,:)]);
+    minV=min([minV,temp2_proj(2,:)]);
+    maxV=max([maxV temp2_proj(2,:)]);
+end
 
-ur = min([1 temp2_proj(1,:)]):max([size(im1,2) temp2_proj(1,:)]) ;
-vr = min([1 temp2_proj(2,:)]):max([size(im1,1) temp2_proj(2,:)]) ;
+ur = minU:maxU ;
+vr = minV:maxV;
 
-[u,v] = meshgrid(ur,vr) ;
+[u,v] = meshgrid(ur,vr); 
 
 %reconstructing  im1
+img1 = imread(images{reference});
+img1=inverse_cylinderical_projection (img1,595,-0.15,0);
+im1=im2single(img1);
 im1_reconst = vl_imwbackward(im2double(im1),u,v) ;
 
-% reconstructing im2
-% (x,y,z)' = (fx/z, fy/z)
-z_proj = H(3,1) * u + H(3,2) * v + H(3,3) ;
-u_proj = (H(1,1) * u + H(1,2) * v + H(1,3)) ./ z_proj ;
-v_proj = (H(2,1) * u + H(2,2) * v + H(2,3)) ./ z_proj ;
-
-%vl_imwbackward returns the pixels of image im2 at positions u_proj and v_proj
-im2_reconst = vl_imwbackward(im2double(im2),u_proj,v_proj) ;
-
-
-%mos=feather_blend(im1,im2);
-%mos=pyramid_blend(im1,im2);
-
-
-pause
-mass = ~isnan(im1_reconst) + ~isnan(im2_reconst) ;
+reconst={};
+for i=1:length(images)
+    if i==reference
+        reconst{i}=im1_reconst;
+    end
+    
+    H=allH{i};
+    
+    inc=1;
+    if i>reference
+        inc=-1;
+    end
+    for ind=i+inc:inc:(reference-inc)
+        H=H*allH{ind};
+    end
+    img2 = im2single(imread(images{i}));
+        
+    % reconstructing im2
+    % (x,y,z)' = (fx/z, fy/z)
+    z_proj = H(3,1) * u + H(3,2) * v + H(3,3) ;
+    u_proj = (H(1,1) * u + H(1,2) * v + H(1,3)) ./ z_proj ;
+    v_proj = (H(2,1) * u + H(2,2) * v + H(2,3)) ./ z_proj ;
+    
+    %vl_imwbackward returns the pixels of image im2 at positions u_proj and v_proj
+    im2_reconst = vl_imwbackward(im2double(img2),u_proj,v_proj) ;
+    reconst{i}=im2_reconst;
+end
+im1_reconst=reconst{1};
 im1_reconst(isnan(im1_reconst)) = 0 ;
-im2_reconst(isnan(im2_reconst)) = 0 ;
-mosaic = (im1_reconst + im2_reconst) ./ mass ;
+top=min(find(im1_reconst(:,end-50)>0));
+slope=top/size(im2_reconst,2);
+for i=2:length(images)
+    im2_reconst=reconst{i};
+    im2_reconst(isnan(im2_reconst)) = 0 ;
+    im1_reconst=pyramid_blend(im1_reconst,im2_reconst);
+end
+%T=[1 -slope; 0 1 ; 0 0 ];
+%t_proj=maketform('affine',T);
+%im1_reconst=mat2gray(imtransform(im1_reconst,t_proj,'FillValues',.3));
 
-figure(2) ; 
-clf ;
-imagesc(mosaic) ; 
+imagesc(im1_reconst)
+%mos=feather_blend(im1,im2);
+%mosaic=pyramid_blend(im1_reconst,im2_reconst);
 
+% mass = ~isnan(im1_reconst) + ~isnan(im2_reconst) ;
+% im1_reconst(isnan(im1_reconst)) = 0 ;
+% im2_reconst(isnan(im2_reconst)) = 0 ;
+% mosaic = (im1_reconst + im2_reconst) ./ mass ;
+
+%imagesc(mosaic) ; 
+H=im1_reconst;
 end
